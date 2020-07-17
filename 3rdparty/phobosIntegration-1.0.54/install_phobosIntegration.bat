65afd33418ae98de16bc99880d59fff11cabd4fd
Ben <benknight135@gmail.com>
2020-07-17 17:53:08 +0100

    move packaging to release folder

--- 3rdparty/phobosIntegration-1.0.54/install_phobosIntegration.bat
+++ 3rdparty/phobosIntegration-1.0.54/install_phobosIntegration.bat
@@ -1,18 +1,44 @@
 @echo off
+
+SETLOCAL
 :: set working directory to script directory
 SET initcwd=%cd%
 SET scriptpath=%~dp0
 cd %scriptpath:~0,-1%
 
-:: get GitHub PAT from first argument
-set token=%1
-:: download phobos integration exe from github release using token
-fetch --repo="https://github.com/i3drobotics/phobosIntegration" --tag="v1.0.54" --release-asset="phobosIntegration-1.0.54-win64_archive.exe" --progress --github-oauth-token=%token% ./
-:: extract self-exracting archive
-phobosIntegration-1.0.54-win64_archive.exe -o"./" -y
-:: delete downloaded file
-del phobosIntegration-1.0.54-win64_archive.exe
-echo Phobos Integration install complete
+:: search for token in first argument
+set token_found=false
+if "%~1"=="" (
+    :: token not stated in argument, look for it in token.txt file
+    echo Reading PAT from 'token.txt'
+    if exist "token.txt" (
+        :: read github PAT from text file
+        set /p token=< token.txt
+        set token_found=true
+    ) else (
+        :: failed to file token.txt file
+        echo Token file not found
+    )
+) else (
+    :: token found in first argument, read token from argument
+    set token=%~1
+    set token_found=true
+)
+
+:: check token was found
+if %token_found%==true (
+    :: download phobos integration exe from github release using token
+    fetch --repo="https://github.com/i3drobotics/phobosIntegration" --tag="v1.0.54" --release-asset="phobosIntegration-1.0.54-win64_archive.exe" --progress --github-oauth-token=%token% ./
+    :: extract self-exracting archive
+    phobosIntegration-1.0.54-win64_archive.exe -o"./" -y
+    :: delete downloaded file
+    del phobosIntegration-1.0.54-win64_archive.exe
+    echo Phobos Integration install complete
+) else (
+    echo Token not found
+)
 
 :: reset working directory
 cd %initcwd%
+
+ENDLOCAL
--- scripts/install_3rdparty.bat
+++ scripts/install_3rdparty.bat
@@ -6,14 +6,11 @@ SET initcwd=%cd%
 SET scriptpath=%~dp0
 cd %scriptpath:~0,-1%
 
-:: read github PAT from text file
-set /p token=< token.txt  
-
 :: install opencv
 call ..\3rdparty\opencv-3.4.1\install_opencv.bat
 
 :: install phobosIntegration
-call ..\3rdparty\phobosIntegration-1.0.54\install_phobosIntegration.bat %token%
+call ..\3rdparty\phobosIntegration-1.0.54\install_phobosIntegration.bat
 
 :: reset working directory
 cd %initcwd%
