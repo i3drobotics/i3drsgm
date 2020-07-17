adc9e32a86f25d3c7f37f5059d8911c47b4be636
Ben <benknight135@gmail.com>
2020-07-17 17:53:08 +0100

    move packaging to release folder

--- scripts/package.bat
+++ scripts/package.bat
@@ -12,25 +12,27 @@ set /p version=< version.txt
 
 :: generate i3drsgm install script
 call update_install_script.bat
-:: copy install script to installer folder
-mkdir ..\installer
-copy install_i3drsgm.bat ..\installer\install_i3drsgm.bat
+:: copy install script to release folder
+mkdir ..\release
+copy install_i3drsgm.bat ..\release\install_i3drsgm.bat
 del install_i3drsgm.bat
+:: copy fetch applicaiton to release folder
+copy ..\3rdparty\phobosIntegration-1.0.54\fetch.exe ..\release\fetch.exe /Y
 
 cd ..\
 
-:: copy cmake directory to installer
+:: copy cmake directory to release folder
 xcopy cmake install\i3drsgm\cmake\ /E/H
-:: copy Phobos Integration download files to installer
+:: copy Phobos Integration download files to release folder
 mkdir install\i3drsgm\phobosIntegration-1.0.54\
 copy 3rdparty\phobosIntegration-1.0.54\install_phobosIntegration.bat install\i3drsgm\phobosIntegration-1.0.54\install_phobosIntegration.bat /Y
 copy 3rdparty\phobosIntegration-1.0.54\fetch.exe install\i3drsgm\phobosIntegration-1.0.54\fetch.exe /Y
-:: copy OpenCV download files to installer
+:: copy OpenCV download files to release folder
 mkdir install\i3drsgm\opencv-3.4.1\
 copy 3rdparty\opencv-3.4.1\install_opencv.bat install\i3drsgm\opencv-3.4.1\install_opencv.bat /Y
 :: create self-extracting exe
 cd install
-%z7path% a -sfx ../installer/i3drsgm-%version%.exe i3drsgm
+%z7path% a -sfx ../release/i3drsgm-%version%.exe i3drsgm
 
 :: remove unneeded files from install folder
 cd ..
