3580da198b0552788ffaf53a57d81fd742081097
Ben <benknight135@gmail.com>
2020-07-17 16:21:47 +0100

    add version varaible to i3drsgm install script

--- scripts/install_i3drsgm.bat
+++ scripts/install_i3drsgm.bat
@@ -4,14 +4,17 @@ SET initcwd=%cd%
 SET scriptpath=%~dp0
 cd %scriptpath:~0,-1%
 
+:: set i3drsgm version
+set version="1.0.1"
+
 :: get GitHub PAT from first argument
 set token=%1
 :: download phobos integration exe from github release using token
-fetch --repo="https://github.com/i3drobotics/i3drsgm" --tag="v1.0.1" --release-asset="i3drsgm-1.0.1.exe" --progress --github-oauth-token=%token% ./
+fetch --repo="https://github.com/i3drobotics/i3drsgm" --tag="v%version%" --release-asset="i3drsgm-%version%.exe" --progress --github-oauth-token=%token% ./
 :: extract self-exracting archive
-i3drsgm-1.0.1.exe -o"./" -y
+i3drsgm-%version%.exe -o"./" -y
 :: delete downloaded file
-del i3drsgm-1.0.1.exe
+del i3drsgm-%version%.exe
 
 :: complete message
 echo I3DRSGM install complete
